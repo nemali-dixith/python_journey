5cc0e3ad754d72878b4dadd0887f45ee166a60e7
N Dixith <nemalidixith392@gmail.com>
2026-04-01 16:30:35 +0530

    add day1 python practice files

---
+++ day1.py
@@ -0,0 +1,11 @@
+name = input("enter the name:")
+age = int(input("enter the age:"))
+cgpa = float(input("enter your cgpa:"))
+is_student = True
+
+print("Name:",name)
+print("Age:",age)
+print("Cgpa:",cgpa)
+print("Is Student:",is_student)
+print("Type of name",type(name))
+print("Type of age:",type(age))
