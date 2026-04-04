c670c0b267a1eab123136a1b77c833b911a0ca03
N Dixith <nemalidixith392@gmail.com>
2026-04-04 20:48:26 +0530

    add day 2- strings and list practice

---
+++ day2.py
@@ -0,0 +1,38 @@
+# string practice
+# name = "Dixith Nemali"
+# print(len(name))
+# print(name.upper())
+# print(name.lower())
+# print(name[0])
+# print(name[-1])
+
+# list practice
+# skills = ["Sql","Python","Power Bi"]
+# skills.append("Ms Excel")
+# print(skills)
+# print(skills[3])
+# print(len(skills))
+
+# String Exercise 1 : convert to tittle case 
+# name = "nemali dixith"
+# print(name.title())
+
+# Exercise 2:count how many words
+# sentence = "I am learning Python"
+# print(sentence.split())
+
+# list Exercise 1 :adding removing then printing 
+# skills = ["Python","Sql","Excel","Power BI"]
+# skills.append("Pandas")
+# skills.remove("Excel")
+# print(skills)
+
+# Exercise 2:sort list,print highest and lowest no
+# numbers = [2,5,6,13,24,9]
+# numbers.sort()
+# print("lowest no:",numbers[0])
+# print("highest no:",numbers[-1])
+
+# Exercise 3:useing slicing
+cities = ["Mumbai","Jamshedpur","Bhubaneswar","Rourkela"]
+print(cities[1:3])
