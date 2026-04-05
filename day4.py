8c07ce5ce29a7f77ecab336e2b6709b5885cdb1e
N Dixith <nemalidixith392@gmail.com>
2026-04-05 22:22:37 +0530

    Add files via upload
    practiced function on day 5

---
+++ day4.py
@@ -0,0 +1,36 @@
+# 1Basic function syntax
+
+# def square(number):
+#     print(number ** 2)
+
+# square(4)
+
+# 2Function with multiple parameters 
+# def add(numOne, numTwo):
+#     return (numOne + numTwo)
+
+# print(add(10,7))
+#3 Polymorphism in functions 
+# def multipfy(numOne,numTwo):
+#     return numOne * numTwo
+
+# print(multipfy(8,6))
+# print(multipfy('g',6))
+# print(multipfy(6,'t'))
+#4 Function returning multiple values 
+# import math
+# def circle_stats(radius):
+#     area = math.pi * radius ** 2
+#     circumference = 2* math.pi * radius
+#     return area , circumference
+
+# area , circumference =circle_stats(8)
+# print("Area:",area,"circumference:",circumference)
+
+#5Default parameter value
+# def greet(name = "User"):
+#     return("Hello,"+ name + "!")
+# print(greet("Dixith"))
+# print(greet())
+
+#6 
